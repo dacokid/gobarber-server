193e0dd88482131b4d3bcd810abdbaedb7541db6
Daniel Cunha <danielcc.jp@gmail.com>
2020-11-07 21:15:09 -0300

    + rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '3d1f871ac137682b9fd182b94805839e',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -33,14 +34,16 @@ class AuthenticationUserService {
       throw new Error('Incorrent email/password combination');
     }
 
-    const token = sign({}, '3d1f871ac137682b9fd182b94805839e', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d'
+      expiresIn,
     });
 
     return {
       user,
-      token
+      token,
     };
   }
 }
