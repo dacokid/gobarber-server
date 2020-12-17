ee913d1bf16f5881b08a2501e1a4ea738e18ec84
Daniel Cunha <danielcc.jp@gmail.com>
2020-12-17 15:24:40 -0300

    + usando controllers

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+interface IUserResponse {
+  name: string;
+  email: string;
+  password?: string;
+}
+export default class UserAvatarController {
+  async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    const user: IUserResponse = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+interface IUserResponse {
+  name: string;
+  email: string;
+  password?: string;
+}
+
+export default class UsersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = container.resolve(CreateUserService);
+
+    const user: IUserResponse = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
