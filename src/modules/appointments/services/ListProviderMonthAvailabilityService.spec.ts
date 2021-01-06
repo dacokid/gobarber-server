64d5b88697c779dfebd91d3b48f5303c35a623cd
Daniel Cunha <danielcc.jp@gmail.com>
2021-01-05 23:29:43 -0300

    + listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -17,12 +17,12 @@ describe('UpdateProfile', () => {
   it('should be able to list the moth availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 3, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 8, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 9, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
@@ -32,12 +32,47 @@ describe('UpdateProfile', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 21, 10, 0, 0),
+      date: new Date(2021, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 20, 17, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 21, 8, 0, 0),
     });
 
     const availability = await listProviderMothAvailability.execute({
       provider_id: 'user',
-      year: 2020,
+      year: 2021,
       month: 5,
     });
 
@@ -45,15 +80,15 @@ describe('UpdateProfile', () => {
       expect.arrayContaining([
         {
           day: 19,
-          available: false,
+          available: true,
         },
         {
           day: 20,
           available: false,
         },
         {
-          day: 221,
-          available: false,
+          day: 21,
+          available: true,
         },
         {
           day: 22,
