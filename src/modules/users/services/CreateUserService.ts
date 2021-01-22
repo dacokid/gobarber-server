bcb4a3f5703793be63e527db5d502f5ba2c4c487
Daniel Cunha <danielcc.jp@gmail.com>
2021-01-22 18:07:50 -0300

    + invalidando cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,7 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '../infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   name: string;
@@ -19,6 +20,8 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -35,6 +38,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -1,5 +1,7 @@
 export default interface ICacheProvider {
   save(key: string, value: any): Promise<void>;
   recover<T>(key: string): Promise<T | null>;
+  // Deletar
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
